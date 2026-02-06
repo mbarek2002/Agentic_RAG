c4f7ef2208ce85f4b32adbcb1aee8c3b0066399d
mbarek2002 <ihebmbarek05@gmail.com>
2026-02-07 00:09:04 +0100

    add paper model

---
+++ src/models/paper.py
@@ -0,0 +1,22 @@
+import uuid
+from datetime import datetime , timezone
+
+from sqlalchemy import JSON , Boolean , Column , DateTime , String , Text
+from sqlalchemy.dialects.postgresql import UUID
+from src.db.interfaces.postgresql import Base
+
+
+class paper(Base):
+    __tablename__ = "papers"
+
+    id = Column(UUID(as_uuid=True) , primary_key=True , default=uuid.uuid4)
+    arxiv_id = Column(String , unique= True , nullable=False , index=True)
+    title = Column(String , nullable=False)
+    authors = Column(JSON , nullable=False)
+    abstract = Column(Text , nullable=False)
+    categories = Column(JSON , nullable=False)
+    published_date = Column(DateTime , nullable=False)
+    pdf_url = Column(String , nullable=False)
+
+    created_at = Column(DateTime , default=lambda : DateTime.now(timezone.utc))
+    updated_at = Column(DateTime , default=lambda : DateTime.now(timezone.utc) , onupdate = lambda : DateTime.now(timezone.utc))
